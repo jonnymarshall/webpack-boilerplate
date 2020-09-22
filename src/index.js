8f3c093c2111ef52a4b1c51c53ae4bc4c26cc61c
Jonny Marshall <jonny@Jonnys-MBP.cust.communityfibre.co.uk>
2020-09-22 15:18:19 +0100

    added TerserPlugin config

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import regeneratorRuntime from "regenerator-runtime";
 import "./style.scss";
+
+const thisIsAVeryLongFunctionName = (thisIsAVeryLongFunctionArgumentName) => {
+  console.log(thisIsAVeryLongFunctionArgumentName);
+};
+
+thisIsAVeryLongFunctionName();
